38acd83ae67ae9bcd87ace969b27c7fd3a45a72f
jgwentworth92 <alphabetboi92@protonmail.com>
2023-11-12 13:20:46 -0500

    updated edit button to use new form actions and useform status

--- src/components/EditButton.tsx
+++ src/components/EditButton.tsx
@@ -1,34 +1,44 @@
 "use client";
+import { EditAction } from "@/server/factories";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { useRef, useState, useTransition } from "react";
+import { useFormStatus } from "react-dom";
 
-import { editToDo } from "@/server/factories"; // Make sure to import your editToDo function
+export interface EditProp {
+  todoId: string;
+  initialName: string;
+  csrfToken: string;
+}
 
-export default function EditButton({ todoId, initialName }) {
-  const [pending, startTransition] = useTransition();
-  const newNameRef = useRef<HTMLInputElement>(null);
-  const [name, setName] = useState(initialName); // Initial name of the todo
-
-  const handleClick = async () => {
-    const updated = await editToDo(todoId, newNameRef.current!.value);
-    setName(updated.name);
-    console.log(updated);
-  };
-
+function Submit() {
+  const status = useFormStatus();
   return (
-    <div>
+    <button
+      className="inline-flex items-center justify-center rounded-full bg-green-600 px-10 py-4 text-center text-base font-normal text-white hover:bg-opacity-90 disabled:bg-gray-500"
+      disabled={status.pending}
+      type="submit"
+    >
+      Edit Todo
+    </button>
+  );
+}
+export default function EditButton({
+  todoId,
+  initialName,
+  csrfToken,
+}: EditProp) {
+  return (
+    <form action={EditAction}>
+      <input type="hidden" name="csrf_token" value={csrfToken} />
+      <input type="hidden" name="todoID" value={todoId} />
       <input
-        ref={newNameRef}
         type="text"
         defaultValue={initialName}
+        name="editTodo"
         className="rounded-lg border border-gray-300 px-4 py-4 text-base font-normal text-gray-600 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600"
       />
-      <button
-        disabled={pending}
-        onClick={async () => startTransition(handleClick)}
-        className="inline-flex items-center justify-center rounded-full bg-green-600 px-10 py-4 text-center text-base font-normal text-white hover:bg-opacity-90 disabled:bg-gray-500"
-      >
-        Edit Todo
-      </button>
-    </div>
+      <Submit />
+    </form>
   );
 }
